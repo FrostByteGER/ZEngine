9657d3c2f887b586be1ccd0711fa1990a818bf2e
jan_schult <jschult@stud.hs-bremen.de>
2017-03-24 11:27:38 +0100

    added IMovalbe

--- SFML_Engine/Engine/SpriteActor.cs
+++ SFML_Engine/Engine/SpriteActor.cs
@@ -1,13 +1,45 @@
 ﻿using System;
+using SFML.System;
 
 namespace SFML_Engine.Engine
 {
     class SpriteActor : ITickableInterface ,IMovable
     {
+        public Vector2f GetAcceleration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetMovable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vector2f GetVelocity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAcceleration(Vector2f acceleration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMovable(bool movable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetVelocity(Vector2f velocity)
+        {
+            throw new NotImplementedException();
+        }
 
         public void Tick(double deltaTime)
         {
             Console.WriteLine("Actor Tick!");
         }
+
+
     }
 }
