c027cae3f135f5af3fa56bf8e54f7dedc3e0cee9
Kevin Kuegler <kekuegler@stud.hs-bremen.de>
2017-03-31 09:48:55 +0200

    Added Move methods to Actorable interface

--- SFML_Engine/Engine/IMovable.cs
+++ SFML_Engine/Engine/IMovable.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using SFML.Graphics;
@@ -14,6 +15,9 @@ namespace SFML_Engine.Engine
         bool Movable { get; set; }
         Vector2f Velocity { get; set; }
         Vector2f Acceleration { get; set; }
+
+	    void Move(float x, float y);
+	    void Move(Vector2f position);
 
     }
 }
