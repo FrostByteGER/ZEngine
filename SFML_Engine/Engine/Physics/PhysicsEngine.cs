c8b2524e03c1858757fdf1a73c68404f45c4a86a
jan_schult <jschult@stud.hs-bremen.de>
2017-03-31 09:36:08 +0200

    start implementing Physics

--- SFML_Engine/Engine/Physics/PhysicsEngine.cs
+++ SFML_Engine/Engine/Physics/PhysicsEngine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using SFML.Graphics;
+using SFML.System;
 
 namespace SFML_Engine.Engine.Physics
 {
@@ -9,9 +10,34 @@ namespace SFML_Engine.Engine.Physics
 
         public readonly float Gravity = 9.81f;
 
+        private SpriteActor CollidableGroup;
+
         internal void PhysicsTick(float deltaTime, ref List<Transformable> actors)
         {
             Console.WriteLine("Physics Tick");
         }
+
+        private void moveActors(float deltaTime, ref List<Transformable> actors)
+        {
+
+            Vector2f VelocityTemp;
+
+            foreach (var actor in actors)
+            {
+                var OneActor = actor as IActorable;
+                //tickableActor?.Tick(deltaTime);
+
+                if (OneActor != null)
+                {
+                    if (OneActor.Movable)
+                    {
+                        
+                    }
+                }
+
+
+            }
+        }
+
     }
 }
