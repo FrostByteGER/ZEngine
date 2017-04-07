ba00f1aba6da2b8a9777892ed07b300700a83716
jan_schult <jschult@stud.hs-bremen.de>
2017-04-07 11:17:25 +0200

    change Actor max MaxAcceleration to -1 MaxVelocity = -1

--- SFML_Engine/Engine/Actor.cs
+++ SFML_Engine/Engine/Actor.cs
@@ -14,7 +14,7 @@ namespace SFML_Engine.Engine
 		public Vector2f Velocity { get; set; }
 		public float MaxVelocity { get; set; } = -1f;
 		public Vector2f Acceleration { get; set; }
-		public float MaxAcceleration { get; set; } = .1f;
+		public float MaxAcceleration { get; set; } = -1f;
 		public float Mass { get; set; } = 1.0f;
 		public List<ActorComponent> Components { get; set; } = new List<ActorComponent>();
 		public bool hasGravity { get; set; } = false;
