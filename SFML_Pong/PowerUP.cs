c040546766800e976fb66c2e3b4c05a1f18fc2a0
Jan Schult <janrippen@ymail.com>
2017-04-11 16:03:27 +0200

    fix PowerUP

--- SFML_Pong/PowerUP.cs
+++ SFML_Pong/PowerUP.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SFML_Engine.Engine;
+using SFML.System;
 
 namespace SFML_Pong
 {
@@ -28,18 +29,22 @@ namespace SFML_Pong
 				if (ss.SphereDiameter < 30)
 				{
 					ss.SphereDiameter += 10;
+					ball.Position = new Vector2f(ball.Position.X - 5, ball.Position.Y - 5);
 				}
 				else if(ss.SphereDiameter > 100)
 				{
 					ss.SphereDiameter -= 10;
+					ball.Position = new Vector2f(ball.Position.X + 5, ball.Position.Y + 5);
 				}
 				else if(r.NextDouble() > 0.5f)
 				{
-					ss.SphereDiameter += (float)r.NextDouble()*10;
+					ss.SphereDiameter += 10;
+					ball.Position = new Vector2f(ball.Position.X - 5, ball.Position.Y - 5);
 				}
 				else
 				{
-					ss.SphereDiameter -= (float)r.NextDouble() * 10;
+					ss.SphereDiameter -= 10;
+					ball.Position = new Vector2f(ball.Position.X + 5, ball.Position.Y + 5);
 				}
 
 				Console.WriteLine("Overlap");
