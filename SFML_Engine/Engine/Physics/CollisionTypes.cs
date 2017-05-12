80c4eeec5867d43e2ab9bafa472c25dac0a25b97
Kevin Kuegler <kekuegler@stud.hs-bremen.de>
2017-05-12 16:18:08 +0200

    Added Simple Collision Enum for Filtering

---
+++ SFML_Engine/Engine/Physics/CollisionTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using BulletSharp;
+
+namespace SFML_Engine.Engine.Physics
+{
+	[Flags]
+	public enum CollisionTypes
+	{
+		AllFilter = -1,
+		None = 0,
+		DefaultFilter = 1,
+		StaticFilter = 2,
+		KinematicFilter = 4
+	}
+}
