c317a1780cc3adb51389e2b5e952a0583f3c4b82
Kevin Kuegler <kevinkuegler@outlook.com>
2017-05-25 20:43:52 +0200

    Added SpaceSEM Shoot'em-Up Project with simple game mechanics.

---
+++ SFML_SpaceSEM/SpaceSEMPersistentGameMode.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML_Engine.Engine;
+using SFML_Engine.Engine.Game;
+using SFML_Engine.Engine.Utility;
+
+namespace SFML_SpaceSEM
+{
+	public class SpaceSEMPersistentGameMode : PersistentGameMode
+	{
+		public uint HighScore { get; set; } = 0;
+		public uint AlltimeHighScore { get; set; } = 0;
+		public uint SecretThreshold { get; } = 9999;
+		public uint CurrentLevel { get; set; } = 1;
+		public uint MaxLevels { get; set; } = 1;
+
+		public static List<string> MusicTracks = new List<string> { "Assets/SFML_SpaceSEM/BGM_Main_1.wav", "Assets/SFML_SpaceSEM/BGM_Main_2.wav", "Assets/SFML_SpaceSEM/BGM_Main_3.wav", "Assets/SFML_SpaceSEM/BGM_Main_4.wav", "Assets/SFML_SpaceSEM/BGM_Main_5.wav" };
+
+		public static string BGM_MLG_Main = "Assets/SFML_SpaceSEM/BGM_MLG_Main.wav";
+		public static string BGM_MLG_Final = "Assets/SFML_SpaceSEM/BGM_MLG_Final.wav";
+		public static string BGM_MLG_Secret = "Assets/SFML_SpaceSEM/BGM_MLG_Secret.wav";
+		public static string BGM_MLG_Current = BGM_MLG_Main;
+
+		public static Texture EnemyTexture { get; set; }
+
+		public static Music BGM_Main;
+
+		public static SoundBuffer PowerUpBuffer { get; set; }
+		public static SoundBuffer BlockHitBuffer { get; set; }
+		public SpaceSEMPersistentGameMode()
+		{
+			PowerUpBuffer = new SoundBuffer("Assets/SFML_SpaceSEM/SFX_PowerUp.wav");
+			Console.WriteLine(PowerUpBuffer.Duration.AsMilliseconds());
+			BlockHitBuffer = new SoundBuffer("Assets/SFML_SpaceSEM/SFX_BlockHit.wav");
+			Console.WriteLine(BlockHitBuffer.Duration.AsMilliseconds());
+
+			BGM_Main = new Music(StartSpaceSEM.MountainDewMode ? BGM_MLG_Current : MusicTracks[EngineMath.EngineRandom.Next(0, MusicTracks.Count)]);
+			BGM_Main.Loop = true;
+			BGM_Main.Volume = Engine.Instance.GlobalVolume;
+
+			EnemyTexture = new Texture("Assets/SFML_SpaceSEM/Enemy_01.png");
+		}
+
+		public static void SwitchMusic()
+		{
+			BGM_Main.Stop();
+			BGM_Main = new Music(StartSpaceSEM.MountainDewMode ? BGM_MLG_Current : MusicTracks[EngineMath.EngineRandom.Next(0, MusicTracks.Count)]);
+			BGM_Main.Loop = true;
+			BGM_Main.Volume = Engine.Instance.GlobalVolume;
+			BGM_Main.Play();
+		}
+	}
+}
---
+++ SFML_SpaceSEM/StartSpaceSEM.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SFML_Engine.Engine;
+using SFML_Engine.Engine.Game;
+using SFML_Engine.Engine.Physics;
+
+namespace SFML_SpaceSEM
+{
+	class StartSpaceSEM
+	{
+		public static bool MountainDewMode { get; set; } = true;
+		public static Engine EngineRef = Engine.Instance;
+		public static PhysicsEngine Physics;
+		public static void Main(string[] args)
+		{
+			if (args.Length >= 1)
+			{
+				MountainDewMode = bool.Parse(args[0]);
+			}
+
+			SpaceSEMMenuLevel.MainGameFont = new SFML.Graphics.Font(MountainDewMode ? "Assets/SFML_SpaceSEM/comic.ttf" : "Assets/SFML_SpaceSEM/arial.ttf");
+
+
+			EngineRef.GameInfo = new SpaceSEMGameInfo();
+			EngineRef.PersistentGameMode = new SpaceSEMPersistentGameMode();
+			EngineRef.EngineWindowWidth = 800;
+			EngineRef.EngineWindowHeight = 800;
+			EngineRef.InitEngine();
+			Physics = EngineRef.PhysicsEngine;
+
+			Physics.AddGroup("Players");
+			Physics.AddGroup("Enemies");
+			Physics.AddGroup("Borders");
+			Physics.AddGroup("Obstacles");
+			Physics.AddGroup("PowerUp");
+			Physics.AddGroup("Bullets");
+
+			// Player/Enemy Collision
+			Physics.AddCollidablePartner("Players", "Enemies");
+			Physics.AddCollidablePartner("Enemies", "Players");
+
+			// Bullet Overlaps
+			Physics.AddOverlapPartners("Players", "Bullets");
+			Physics.AddOverlapPartners("Bullets", "Players");
+			Physics.AddOverlapPartners("Enemies", "Bullets");
+			Physics.AddOverlapPartners("Bullets", "Enemies");
+
+			// Border Collision
+			Physics.AddCollidablePartner("Players", "Borders");
+			Physics.AddCollidablePartner("Enemies", "Borders");
+			Physics.AddCollidablePartner("Bullets", "Borders");
+			Physics.AddCollidablePartner("PowerUp", "Borders");
+			Physics.AddCollidablePartner("Obstacles", "Borders");
+
+			// Obstacle Collision
+			Physics.AddCollidablePartner("Players", "Obstacles");
+			Physics.AddCollidablePartner("Obstacles", "Players");
+
+			// Powerup Overlaps
+			Physics.AddOverlapPartners("PowerUp", "Players");
+
+
+			var menuLevel = new SpaceSEMMenuLevel();
+			Engine.Instance.RegisterLevel(menuLevel);
+			menuLevel.InitiateMenu();
+			EngineRef.LoadLevel(menuLevel);
+
+			var gameLevel = new SpaceSEMGameLevel();
+			Engine.Instance.RegisterLevel(gameLevel);
+
+			EngineRef.StartEngine();
+			
+			SpaceSEMMenuLevel.MainGameFont.Dispose(); // I swear, one day I'll write a Manager that destroys every game object on engine shutdown...
+			Console.ReadLine();
+		}
+
+		public static List<List<float[]>> LoadBlockPositions()
+		{
+			List<List<float[]>> values = new List<List<float[]>>();
+			try
+			{
+				using (StreamReader sr = new StreamReader("Assets/SFML_Breakout/Levels.cfg"))
+				{
+					string line;
+					List<float[]> levelValues = new List<float[]>();
+					float[] pair = new float[2];
+					while ((line = sr.ReadLine()) != null)
+					{
+						if (line == "---")
+						{
+							values.Add(levelValues);
+							levelValues = new List<float[]>();
+						}
+						else
+						{
+							line = line.Remove(line.Length - 1);
+							var valuesraw = line.Split(',');
+							pair[0] = Convert.ToSingle(valuesraw[0]);
+							pair[1] = Convert.ToSingle(valuesraw[1]);
+							levelValues.Add(pair);
+							pair = new float[2];
+						}
+					}
+					values.Add(levelValues);
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("The file could not be read:");
+				Console.WriteLine(e.Message);
+			}
+			return values;
+		}
+	}
+}
