e4a96b9ee4d5678caae703285a1db014d9d19f2e
Kevin Kuegler <kekuegler@stud.hs-bremen.de>
2017-06-16 10:43:42 +0200

    Fixed Property bug in Level. Added AnimationSprite and AnimationComponent. Needs testing.

---
+++ SFML_Engine/Engine/Graphics/AnimationComponent.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using SFML.Graphics;
+using SFML_Engine.Engine.Utility;
+
+namespace SFML_Engine.Engine.Graphics
+{
+	public class AnimationComponent : RenderComponent
+	{
+		private AnimationSprite _animSprite;
+
+		public AnimationSprite AnimSprite
+		{
+			get => _animSprite;
+			set
+			{
+				_animSprite = value;
+				Origin = new TVector2f(AnimSprite.GetGlobalBounds().Width / 2.0f, AnimSprite.GetGlobalBounds().Height / 2.0f);
+				ComponentBounds = Origin;
+			}
+		}
+
+		public override TVector2f LocalPosition
+		{
+			get => AnimSprite.Position;
+			set => base.LocalPosition = value;
+		}
+
+		public override float LocalRotation
+		{
+			get => AnimSprite.Rotation;
+			set
+			{
+				base.LocalRotation = value;
+				AnimSprite.Rotation = value;
+			}
+		}
+
+		public override TVector2f LocalScale
+		{
+			get => AnimSprite.Scale;
+			set
+			{
+				base.LocalScale = value;
+				AnimSprite.Scale = value;
+			}
+		}
+
+		public override TVector2f Origin
+		{
+			get => AnimSprite.Origin;
+			set
+			{
+				base.Origin = value;
+				AnimSprite.Origin = value;
+			}
+		}
+
+		public AnimationComponent(Texture spriteSheet, int frameWidth, int frameHeight)
+		{
+			AnimSprite = new AnimationSprite(spriteSheet, frameWidth, frameHeight);
+		}
+
+		public AnimationComponent(AnimationSprite sprite)
+		{
+			AnimSprite = sprite ?? throw new ArgumentNullException(nameof(sprite));
+		}
+
+		public override void Draw(RenderTarget target, RenderStates states)
+		{
+			states.Shader = ComponentMaterial.MaterialShader;
+			target.Draw(AnimSprite, states);
+		}
+
+		public override void Tick(float deltaTime)
+		{
+			base.Tick(deltaTime);
+			if (AnimSprite.State == AnimationState.Running)
+			{
+				AnimSprite.RemainingTime -= deltaTime;
+				if (AnimSprite.RemainingTime <= 0.0)
+				{
+					AnimSprite.NextFrame();
+					AnimSprite.RemainingTime = AnimSprite.FrameDuration;
+				}
+			}
+			AnimSprite.Position = WorldPosition;
+		}
+
+		public override void Destroy(bool disposing)
+		{
+			base.Destroy(disposing);
+			AnimSprite?.Dispose();
+			//Sprite?.Texture?.Dispose();
+		}
+	}
+}
---
+++ SFML_Engine/Engine/Graphics/AnimationSprite.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using SFML.Graphics;
+using SFML_Engine.Engine.Game;
+
+namespace SFML_Engine.Engine.Graphics
+{
+	public class AnimationSprite : Sprite
+	{
+		public Texture AnimationSheet { get; }
+		public int FrameWidth { get; } = 0;
+		public int FrameHeight { get; } = 0;
+		public int FrameRowPosition { get; protected set; } = 0;
+		public int FrameColumnPosition { get; protected set; } = 0;
+		public float FrameDuration { get; set; } = 1.0f;
+		public float RemainingTime { get; protected internal set; } = 1.0f;
+		public bool AutoRepeat { get; set; } = true;
+		public bool PlayBackwards { get; set; } = false;
+
+		public AnimationSprite(Texture spriteSheet, int frameWidth, int frameHeight)
+		{
+			AnimationSheet = spriteSheet;
+			FrameWidth = frameWidth;
+			FrameHeight = frameHeight;
+		}
+
+		public void NextFrame()
+		{
+			if (PlayBackwards)
+			{
+				// If we are at the Texturesheets start, set the Positions to the end.
+				if (FrameRowPosition * FrameWidth == 0 &&
+					FrameColumnPosition * FrameHeight == 0)
+				{
+					// When we are at the start and don't want to repeat, stop the animation and return.
+					if (!AutoRepeat)
+					{
+						StopAnimation();
+						return;
+					}
+					FrameRowPosition = (int) (AnimationSheet.Size.X - FrameWidth);
+					FrameColumnPosition = (int) (AnimationSheet.Size.X - FrameHeight);
+				}
+				else
+				{
+					// If we reached the row start, move a row up.
+					if (FrameRowPosition * FrameWidth == 0)
+					{
+						FrameRowPosition = (int)(AnimationSheet.Size.X - FrameWidth);
+						--FrameColumnPosition;
+					}
+					else
+					{
+						--FrameRowPosition;
+					}
+				}
+			}
+			else
+			{
+				// If we are at the Texturesheets end, set the Positions to the start.
+				if (FrameRowPosition * FrameWidth == AnimationSheet.Size.X - FrameWidth &&
+				    FrameColumnPosition * FrameHeight == AnimationSheet.Size.X - FrameHeight)
+				{
+					// When we are at the end and don't want to repeat, stop the animation and return.
+					if (!AutoRepeat)
+					{
+						StopAnimation();
+						return;
+					}
+					FrameRowPosition = 0;
+					FrameColumnPosition = 0;
+				}
+				else
+				{
+					// If we reached the row end, move a row down.
+					if (FrameRowPosition * FrameWidth == AnimationSheet.Size.X - FrameWidth)
+					{
+						FrameRowPosition = 0;
+						++FrameColumnPosition;
+					}
+					else
+					{
+						++FrameRowPosition;
+					}
+				}
+			}
+			SetSpriteTexture();
+		}
+
+		private void SetSpriteTexture()
+		{
+			TextureRect = new IntRect(FrameRowPosition * FrameWidth, FrameColumnPosition * FrameHeight, FrameWidth, FrameHeight);
+		}
+
+		public AnimationState State { get; private set; } = AnimationState.None;
+
+		public void StartAnimation()
+		{
+			State = AnimationState.Running;
+		}
+
+		public void PauseAnimation()
+		{
+			State = AnimationState.Paused;
+		}
+
+		public void ResumeAnimation()
+		{
+			State = AnimationState.Running;
+		}
+
+		public void TogglePauseAnimation()
+		{
+			switch (State)
+			{
+				case AnimationState.Running:
+					PauseAnimation();
+					break;
+				case AnimationState.Paused:
+					ResumeAnimation();
+					break;
+			}
+		}
+
+		public void StopAnimation()
+		{
+			State = AnimationState.Stopped;
+		}
+
+		public void RestartAnimation()
+		{
+			StopAnimation();
+			StartAnimation();
+		}
+	}
+
+	public enum AnimationState
+	{
+		None,
+		Stopped,
+		Running,
+		Paused
+	}
+}
