5849e9031684370000beb93ff11be917f1b4cb27
Kevin Kuegler <kekuegler@stud.hs-bremen.de>
2017-06-16 14:50:39 +0200

    Added Example Animation Component.

--- SFML_TowerDefense/Source/StartTowerDefense.cs
+++ SFML_TowerDefense/Source/StartTowerDefense.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using SFML.Graphics;
 using SFML_Engine.Engine.Core;
+using SFML_Engine.Engine.Game;
+using SFML_Engine.Engine.Graphics;
 
 namespace SFML_TowerDefense.Source
 {
@@ -13,6 +15,18 @@ namespace SFML_TowerDefense.Source
 			engine.EngineWindowHeight = 800;
 			engine.GameInfo = new TDGameInfo();
 			engine.InitEngine();
+
+			var testLevel = new Level();
+			var animActor = new Actor(testLevel);
+			animActor.SetRootComponent(new AnimationComponent(new Texture("Assets/Game/Animations/ExampleSheet.png"),100,100));
+
+
+			testLevel.RegisterActor(animActor);
+
+			var pc = new PlayerController();
+			testLevel.RegisterPlayer(pc);
+
+			engine.LoadLevel(testLevel);
 
 			engine.StartEngine();
 			Console.ReadLine();
