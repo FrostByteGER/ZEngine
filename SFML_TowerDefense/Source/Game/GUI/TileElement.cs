40b0eac4e4040df36bc71db5ae699c0f24e27588
Jan Schult <janrippen@ymail.com>
2017-06-22 21:21:53 +0200

    add TileElement

---
+++ SFML_TowerDefense/Source/Game/GUI/TileElement.cs
@@ -0,0 +1,19 @@
+﻿using SFML_Engine.Engine.JUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFML_TowerDefense.Source.Game.GUI
+{
+	public class TileElement : JCheckbox
+	{
+
+		public TDTile tile;
+
+		public TileElement(JGUI gui) : base(gui)
+		{
+		}
+	}
+}
